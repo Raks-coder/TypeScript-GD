b5ae55de08a495d678b7bcf5b6c5e5301a83395c
Rakshit <raks.malhotra@rediffmail.com>
2021-07-25 18:24:11 +0530

    Create optionalParams.ts

---
+++ optionalParams.ts
@@ -0,0 +1,12 @@
+
+// ES6 Feature demonstrating default value of params
+var container=document.getElementById('container');
+function countDown (initial,final=0,interval=1) {
+    var current=initial;
+    while(current>final)
+    {
+        container.innerHTML=current;
+        current-=interval;
+
+    }
+}
