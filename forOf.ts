1415735305066de97a081261b133d16e93137df8
Rakshit <raks.malhotra@rediffmail.com>
2021-07-25 19:14:25 +0530

    For...of in typescript

---
+++ forOf.ts
@@ -0,0 +1,10 @@
+var array = [
+  "Pick up Drycleaning",
+  "Save Gotham City",
+  "Repair car"
+]
+
+for (var index in array) {
+  var value = array[index];
+  console.log(`${index}:${value}`); // This is an example of templating in ES6 pretty similar to Angular js binding
+}
